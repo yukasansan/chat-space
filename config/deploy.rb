00386c6b65d29a286e71041164ba345a39a0e1ac
yukasansan <64906781+yukasansan@users.noreply.github.com>
2020-05-29 11:12:33 +0900

    test2

--- config/deploy.rb
+++ config/deploy.rb
@@ -40,9 +40,11 @@ namespace :deploy do
   desc 'Config bundler'
   task :config_bundler do
     on roles(/.*/) do
-      execute 'bundle config --local deployment true'
-      execute 'bundle config --local without "development test"'
-      execute 'bundle config --local path vendor'
+      within release_path do
+        execute :bundle, :config, '--local deployment true'
+        execute :bundle, :config, '--local without "development test"'
+        execute :bundle, :config, "--local path #{shared_path.join('bundle')}"
+      end
     end
   end
 end
