17a242e0f0c0745a29b4dff0289284f07c210753
yukasansan <64906781+yukasansan@users.noreply.github.com>
2020-05-29 19:34:46 +0900

    css

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -3,7 +3,7 @@ $(function() {
     let html = `
       <div class="chat-group-user clearfix">
         <p class="chat-group-user__name">${user.name}</p>
-        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+        <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}" style="float:right">追加</div>
       </div>
     `;
     $("#user-search-result").append(html);
